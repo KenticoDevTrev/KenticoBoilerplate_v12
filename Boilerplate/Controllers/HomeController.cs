8356422219f358b84808becb169502d79aa10614
Trevor Fayas <tfayas@hbs.net>
2019-06-28 16:25:18 -0500

    Added MVC Template Examples for Home Page, along with added Blank Widget Template as option for MVC Widget Page for an example.

--- Boilerplate/Controllers/HomeController.cs
+++ Boilerplate/Controllers/HomeController.cs
@@ -1,28 +1,39 @@
-﻿using CMS.DataEngine;
-using CMS.DocumentEngine;
-using Kentico.PageBuilder.Web.Mvc;
-using Kentico.Web.Mvc;
-using KMVCHelper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Boilerplate.Controllers;
+using CMS.DocumentEngine;
 
-namespace Controllers
+using Kentico.PageBuilder.Web.Mvc;
+using Kentico.PageBuilder.Web.Mvc.PageTemplates;
+using Kentico.Web.Mvc;
+using KMVCHelper;
+
+namespace Boilerplate.Controllers
 {
-    [KMVCRouteOverPathPriority]
-    public class HomeController : BaseController
+    public class HomeController : PageTemplateController
     {
         // GET: Home
         public ActionResult Index()
         {
-            // Get the current page, in this case we know it's the Home page
-            var HomePage = DocumentQueryHelper.RepeaterQuery(Path: "/Home", ClassNames: "MVC.Home", CacheItemName: "HomePage").GetTypedResult().Items.FirstOrDefault();
-            HttpContext.Kentico().PageBuilder().Initialize(HomePage.DocumentID);
-            SetContext(HomePage.DocumentID);
-            return View();
+            // Uncomment and optionally adjust the document query sample when using Page builder on the Home page
+            // See ~/App_Start/ApplicationConfig.cs, ~/Views/Shared/_Layout.cshtml and ~/Views/Home/Index.cshtml
+            // In the administration UI, create a Page type and set its
+            //   URL pattern = '/Home'
+            //   Use Page tab = True
+            // In the administration UI, create a Page utilizing the new Page type
+
+            TreeNode page = DocumentHelper.GetDocuments().Path("/Home").OnCurrentSite().TopN(1).FirstOrDefault();
+            if (page == null)
+            {
+                return HttpNotFound();
+            }
+
+            HttpContext.Kentico().PageBuilder().Initialize(page.DocumentID);
+
+            return new TemplateResult(page.DocumentID);
         }
     }
-
 }
---
+++ Boilerplate/Controllers/PageTemplates/HomePageTemplateFilter.cs
@@ -0,0 +1,28 @@
+﻿using Kentico.PageBuilder.Web.Mvc.PageTemplates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Boilerplate.Controllers.PageTemplates
+{
+    public class HomePageTemplateFilter : IPageTemplateFilter
+    {
+        public IEnumerable<PageTemplateDefinition> Filter(IEnumerable<PageTemplateDefinition> pageTemplates, PageTemplateFilterContext context)
+        {
+            // Applies filtering to a collection of page templates based on the page type of the currently edited page
+            if (context.PageType.Equals("MVC.Home", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Filters the collection to only contain filters allowed for landing pages
+                return pageTemplates.Where(t => GetHomeTemplates().Contains(t.Identifier));
+            }
+
+            // Excludes all landing page templates from the collection if the context does not match this filter
+            // Assumes that the categories of page templates are mutually exclusive
+            return pageTemplates.Where(t => !GetHomeTemplates().Contains(t.Identifier));
+        }
+
+        // Gets all page templates that are allowed for landing pages
+        public IEnumerable<string> GetHomeTemplates() => new string[] { "Home.Template1", "Home.Template2", "Home.Template3" };
+    }
+}
