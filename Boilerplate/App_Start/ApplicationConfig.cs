16034a482b586efe73bb7694173b649671ba077e
Trevor Fayas <tfayas@hbs.net>
2019-07-05 20:36:08 -0500

    Removed my default section so default Kentico one is used.

--- Boilerplate/App_Start/ApplicationConfig.cs
+++ Boilerplate/App_Start/ApplicationConfig.cs
@@ -13,13 +13,7 @@ public class ApplicationConfig
         // Enable required Kentico features
         builder.UsePreview();
         // Registers the Page Builder with the default section we created
-        builder.UsePageBuilder(new PageBuilderOptions()
-        {
-            // Specifies a default section for the page builder feature
-            DefaultSectionIdentifier = "Sections.DefaultSection",
-            // Disables the system's built-in 'Default' section
-            RegisterDefaultSection = false
-        });
+        builder.UsePageBuilder(new PageBuilderOptions());
 
         // Enables the alternative URLs feature
         builder.UsePageRouting(new PageRoutingOptions
