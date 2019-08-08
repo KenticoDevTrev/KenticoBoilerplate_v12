f8b4e2504275d5718a2b69916173e8baa6ea24b4
Trevor Fayas <tfayas@hbs.net>
2019-08-08 15:39:58 -0500

    Fix bug with Kentico Form Widget that would not work with dynamic routing due to the request URL being the same as the page, but the controller being set to the Kentico Form Widget

--- Boilerplate/Library/Dynamic Route Handling/KMVCPageFoundConstraint.cs
+++ Boilerplate/Library/Dynamic Route Handling/KMVCPageFoundConstraint.cs
@@ -19,6 +19,14 @@ namespace KMVCHelper
 
         public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
         {
+
+            // Don't use dynamic routing for this {controller}/* route. 'KenticoFormWidget' is the OOB Kentico Forms widget in the default installation.
+            string ControllerName = (values.ContainsKey("controller") ? ValidationHelper.GetString(values["controller"], "") : "");
+            if (ControllerName.Equals("KenticoFormWidget", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return false;
+            }
+
             // If no document found anyway, then it will always be a match
             TreeNode FoundDoc = DocumentQueryHelper.GetNodeByAliasPath(EnvironmentHelper.GetUrl(httpContext.Request));
             return (FoundDoc != null && (FoundDoc.NodeAliasPath != "/" || !IgnoreRootPage));
